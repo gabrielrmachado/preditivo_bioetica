22ef5ccfaf0a74af79cacbe7624a5a331af6f2a4
gabrielrmachado <gabriel.rmachado10@gmail.com>
2020-07-11 09:34:19 -0300

    ROC Curve included.

--- trabalho_preditivo.py
+++ trabalho_preditivo.py
@@ -10,7 +10,8 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import StratifiedKFold
 from sklearn import metrics
-
+import scikitplot as skplt
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     df = pd.read_excel("dados_oswaldo_preditivo.xlsx")
@@ -28,8 +29,8 @@ if __name__ == "__main__":
     #                     max_iter=1000, momentum=0.9, verbose=False, early_stopping=False, n_iter_no_change=10)
 
     # clf = RandomForestClassifier(n_estimators=9, criterion='entropy')
-    # clf = DecisionTreeClassifier(criterion='entropy', splitter='best', max_features='auto')
-    clf = LogisticRegression(tol=1e-7, solver='liblinear', max_iter=1000)
+    clf = DecisionTreeClassifier(criterion='entropy', splitter='best', max_features='auto')
+    # clf = LogisticRegression(tol=1e-7, solver='liblinear', max_iter=1000)
     accuracies = []
 
     x_train, x_test, y_train, y_test = train_test_split(x_pca, y, test_size=0.2, stratify = y)
@@ -41,12 +42,15 @@ if __name__ == "__main__":
 
         clf = clf.fit(X_train, Y_train)
         accuracies.append(clf.score(X_test, Y_test))
+        y_pred = clf.predict(x_test)
 
     print("Mean accuracy: {0:.3f} (+/- {1:.3f})\n".format(stat.mean(accuracies), stat.stdev(accuracies)))
     print("ACC: {0:.3f}".format(clf.score(x_test, y_test)))
-    print("F1 Score: {0:.3f}".format(metrics.f1_score(y_test, clf.predict(x_test))))
-    print("AUC score: {0:.3f}".format((metrics.roc_auc_score(y_test, clf.predict(x_test)))))
-    print("Confusion Matrix:\n{0}".format(metrics.confusion_matrix(y_test, clf.predict(x_test))))
+    print("F1 Score: {0:.3f}".format(metrics.f1_score(y_test, y_pred)))
+    print("AUC score: {0:.3f}".format((metrics.roc_auc_score(y_test, y_pred))))
+    print("Confusion Matrix:\n{0}".format(metrics.confusion_matrix(y_test, y_pred)))
+    skplt.metrics.plot_roc_curve(y_test, clf.predict_proba(x_test))
+    plt.show()
 
     # print(np.count_nonzero(x_test == 0))
     # print(np.count_nonzero(x_test == 1))
