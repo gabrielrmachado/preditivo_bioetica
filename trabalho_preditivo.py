1f28f7eaed71b71be0a020bf928f67a166af95f5
gabrielrmachado <gabriel.rmachado10@gmail.com>
2020-07-10 22:21:17 -0300

    Inclusion of more classification models.

--- trabalho_preditivo.py
+++ trabalho_preditivo.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import statistics as stat
 from sklearn.neural_network import MLPClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
@@ -18,31 +20,32 @@ if __name__ == "__main__":
     x = df.loc[:, 'A1':'A8'].values
     y = df[["Classe"]].values
 
-    # x_norm = StandardScaler().fit_transform(x)
-
-    # pca = PCA(n_components=8)
-    # x_pca = pca.fit_transform(X = x)
-    # print(x_norm)
+    x_norm = StandardScaler().fit_transform(x)
+    pca = PCA(n_components=8)
+    x_pca = pca.fit_transform(X = x_norm)
     
-    clf = MLPClassifier(hidden_layer_sizes=(20,), activation='logistic', solver='sgd', learning_rate='adaptive', 
-                        learning_rate_init=0.01, max_iter=500, momentum=0.9, tol=1e-5)
+    # clf = MLPClassifier(hidden_layer_sizes=(20,), activation='logistic', solver='adam', learning_rate='adaptive', learning_rate_init=0.01,
+    #                     max_iter=1000, momentum=0.9, verbose=False, early_stopping=False, n_iter_no_change=10)
+
+    # clf = RandomForestClassifier(n_estimators=9, criterion='entropy')
+    # clf = DecisionTreeClassifier(criterion='entropy', splitter='best', max_features='auto')
+    clf = LogisticRegression(tol=1e-7, solver='liblinear', max_iter=1000)
     accuracies = []
 
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, stratify = y)
+    x_train, x_test, y_train, y_test = train_test_split(x_pca, y, test_size=0.2, stratify = y)
     skf = StratifiedKFold(n_splits=10)
     
     for train_idx, test_idx in skf.split(x_train, y_train):
         X_train, X_test = x_train[train_idx], x_train[test_idx]
         Y_train, Y_test = y_train[train_idx], y_train[test_idx]
 
-        clf.fit(X_train, Y_train)
+        clf = clf.fit(X_train, Y_train)
         accuracies.append(clf.score(X_test, Y_test))
 
     print("Mean accuracy: {0:.3f} (+/- {1:.3f})\n".format(stat.mean(accuracies), stat.stdev(accuracies)))
-    print(clf.score(x_test, y_test))
-    print(metrics.roc_auc_score(x_test, y_test))
-
-
+    print("ACC: {0:.3f}".format(clf.score(x_test, y_test)))
+    print("F1 Score: {0:.3f}".format(metrics.f1_score(y_test, clf.predict(x_test))))
+    print("AUC score: {0:.3f}".format((metrics.roc_auc_score(y_test, clf.predict(x_test)))))
 
     # print(np.count_nonzero(x_test == 0))
     # print(np.count_nonzero(x_test == 1))
